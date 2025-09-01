801528aacd11a9dc7cec530440044586800225ee
Jose Caliz <acaliz@unal.edu.co>
2025-09-01 17:51:45 -0400

    Adding accents for my linux keyboard

---
+++ keyboards/ymdk/ymd40/air40/keymaps/jcaliz/rules.mk
@@ -0,0 +1,16 @@
+EXTRAFLAGS += -flto
+MOUSEKEY_ENABLE = no
+AUDIO_SUPPORTED = no
+
+## Debug
+# CONSOLE_ENABLE = yes # enable debug
+# KEYCODE_STRING_ENABLE = yes # enable keycode string for easier debugging
+
+# RGB
+RGB_MATRIX_CUSTOM_USER = yes
+RGB_MATRIX_ENABLE = yes
+RGBLIGHT_SUPPORTED = no
+
+# Unicode
+UNICODE_COMMON = yes # Be able to use unicode in your keyboard
+# UNICODEMAP_ENABLE = yes # be able to use the unicode map variable in the keymap
