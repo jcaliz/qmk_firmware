40bb2cafa3771bcefa080b6834e128457e3bd6bf
Jose Caliz <acaliz@unal.edu.co>
2025-10-29 21:32:23 -0400

    curio with correct layout

--- keyboards/projectcain/curio/keymaps/default/keymap.c
+++ keyboards/projectcain/curio/keymaps/default/keymap.c
@@ -7,55 +7,58 @@
   *
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  q
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   * GNU General Public License for more details.
   * */
 
 #include QMK_KEYBOARD_H
-enum layers{
-  BASE,
-  NUM,
-  SYM,
-  META
+enum layers {
+  _DVORAK,
+  _LRAISE,
+  _RRAISE,
+  _SYSTEM,
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOTE,          LT(SYM, KC_ENT),
-    RCTL_T(S(KC_9)),        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,          S(KC_0),
-         KC_RCTL, KC_LGUI, KC_LALT,     MO(1), SFT_T(KC_BSPC), KC_SPACE,  KC_SPACE,  MO(2),            KC_LEFT, KC_DOWN, KC_RIGHT
-  ),
+    [_DVORAK] = LAYOUT(
+        KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,         KC_Y,    KC_F,    KC_G,   KC_C,     KC_R,     KC_L,   KC_BSPC,
+        KC_ESC,   KC_A,     KC_O,     KC_E,     KC_U,         KC_I,    KC_D,    KC_H,   KC_T,     KC_N,     KC_S,   KC_ENT,
+        KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,     KC_K,         KC_X,    KC_B,    KC_M,   KC_W,     KC_V,     KC_Z,   KC_RSFT,
+        KC_LCTL,  KC_LALT,  KC_LGUI,  MO(_LRAISE),    KC_SPC, KC_SPC,   KC_SPC,  MO(_RRAISE),  KC_LEFT, KC_DOWN,  KC_UP
+    ),
 
-  [NUM] = LAYOUT(
-    KC_TRNS,    KC_PGUP,    KC_HOME,    KC_UP,    KC_END,    KC_WBAK,    KC_PSLS,    KC_7,    KC_8,    KC_9,    KC_PMNS,    KC_DEL,    KC_TRNS,
-    KC_TRNS,    KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_WFWD,    KC_PAST,    KC_4,    KC_5,    KC_6,    KC_PPLS,             KC_TRNS,
-    KC_LBRC,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_0,    KC_1,    KC_2,    KC_3,    KC_PDOT,             KC_RBRC,
-        KC_RCTL, KC_LGUI, KC_LALT,     MO(1), SFT_T(KC_BSPC), KC_SPACE,  KC_SPACE,  MO(2),            KC_LEFT, KC_DOWN, KC_RIGHT
-  ),
+    [_LRAISE] = LAYOUT(
+        RCS(KC_TAB),  LGUI(KC_R),  KC_3,     KC_2,     KC_1,     LCTL(KC_TAB),  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,
+        KC_DEL,       LGUI(KC_C),  KC_6,     KC_5,     KC_4,     KC_NO,         KC_NO,    KC_EQL,  KC_LBRC,  KC_RBRC,  KC_SLSH,  KC_TRNS,
+        KC_TRNS,      LGUI(KC_V),  KC_9,     KC_8,     KC_7,     KC_0,          KC_NO,    KC_LT,   KC_GT,    KC_BSLS,  KC_BSLS,  KC_TRNS,
+        KC_TRNS,      LGUI(KC_W),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,  MO(_SYSTEM),   KC_MNXT,  KC_VOLD, KC_TRNS, KC_TRNS
+    ),
 
-  [SYM] = LAYOUT(
-    KC_TRNS,    KC_PGUP,    KC_HOME,    KC_UP,    KC_END,    KC_WBAK,    KC_PSLS,    KC_7,    KC_8,    KC_9,    KC_PMNS,    KC_DEL,    KC_TRNS,
-    KC_TRNS,    KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_WFWD,    KC_PAST,    KC_4,    KC_5,    KC_6,    KC_PPLS,             KC_TRNS,
-    KC_LBRC,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_0,    KC_1,    KC_2,    KC_3,    KC_PDOT,             KC_RBRC,
-        KC_RCTL, KC_LGUI, KC_LALT,     MO(1), SFT_T(KC_BSPC), KC_SPACE,  KC_SPACE,  MO(2),            KC_LEFT, KC_DOWN, KC_RIGHT
-  ),
+    [_RRAISE] = LAYOUT(
+        KC_GRV,   KC_EXLM,  KC_AT,    KC_HASH,  KC_PERC,  KC_LCBR,     KC_RCBR,  KC_AMPR,  KC_CIRC,  LSFT(KC_8),  KC_QUES,
+        KC_DEL,   KC_QUOT,  KC_LPRN,  KC_RPRN,  KC_UNDS,  KC_MINS,     KC_DLR,   KC_EQL,   KC_LBRC,  KC_RBRC,     KC_SLSH,  KC_TRNS,
+        KC_TRNS,  KC_PPLS,  KC_NO,    KC_NO,    KC_NO,    LALT(KC_E),     KC_TILD,  KC_LT,    KC_GT,    KC_BSLS,     KC_PIPE,  KC_TRNS,
+        KC_1,     KC_TRNS,  KC_TRNS,  KC_TRNS,  MO(_SYSTEM),    KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_MNXT,  KC_VOLD, KC_TRNS,
+    ),
 
-//   [SYM] = LAYOUT(
-//     RGB_TOG,      S(KC_GRV),    KC_GRV,    KC_BSLS,    S(KC_BSLS),    KC_TRNS,    KC_TRNS,    S(KC_MINS),    KC_EQL,    KC_TRNS,    C(KC_W),    C(KC_T),   KC_TRNS,
-//     KC_TRNS,    S(KC_1),    S(KC_2),    S(KC_3),    S(KC_4),    S(KC_5),    S(KC_6),    S(KC_7),    S(KC_8),    KC_SCLN,    S(KC_SCLN),          KC_TRNS,
-//     S(KC_LBRC),    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,             S(KC_RBRC),
-//           KC_RCTL, KC_LGUI, KC_LALT,     MO(1), SFT_T(KC_BSPC), KC_SPACE,  KC_SPACE,  MO(2),            KC_LEFT, KC_DOWN, KC_RIGHT
-//   ),
+    [_SYSTEM] = LAYOUT(
+        QK_BOOT,  UG_TOGG,  UG_HUEU,  UG_SATU,  UG_VALU,  DF(_DVORAK),  KC_TRNS,  KC_F7,    KC_F8,  KC_F9,  KC_F10,
+        KC_TRNS,   UG_NEXT,  UG_HUED,  UG_SATD,  UG_VALD,  KC_TRNS,  AG_SWAP,  KC_F4,    KC_F5,  KC_F6,  KC_F11,  UC_LINX,
+        KC_TRNS,  UG_PREV,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_F1,    KC_F2,  KC_F3,  KC_F12,  UC_NEXT,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_MRWD,  KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+
 
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [BASE] =   { ENCODER_CCW_CW(KC_UP, KC_DOWN),   ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) },
-    [NUM] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),   ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [SYM] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),   ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [_DVORAK] =   { ENCODER_CCW_CW(KC_UP, KC_DOWN),   ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) },
+    [_LRAISE] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),   ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [_RRAISE] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),   ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [_SYSTEM] =   { ENCODER_CCW_CW(KC_UP, KC_DOWN),   ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) },
 };
 #endif
 
@@ -71,3 +74,12 @@ combo_t key_combos[COMBO_COUNT] = {
   [KL_SLSH] = COMBO(kl_combo, S(KC_SLSH)),
   [JK_MINUS] = COMBO(jk_combo, KC_MINUS)
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("kc: %s\n", get_keycode_string(keycode));
+    uprintf("col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+  return true;
+}
